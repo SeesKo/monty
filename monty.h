5c51110b8b7248eaf9ebd9f99cadd0c62c63d626
Siskosee <seeskosee@seeskoseesko.com>
2023-12-14 03:16:17 -0800

    main header

--- monty.h
+++ monty.h
@@ -40,7 +40,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* USER-DEFINED FUNCTION PROTOTYPES */
+/* USER-DEFINED FUNCTION PROTOTYPE */
 void push(stack_t **stack, int line_number, char *args);
 void pall(stack_t **stack, unsigned int line_number);
 void tokenize_line(char *line, stack_t **stack, unsigned int line_number);
