c5cae930332d11d525ccf52202bda66ab63343ee
Siskosee <seeskosee@seeskoseesko.com>
2023-12-15 04:48:35 -0800

    opcode handling

--- instructions.c
+++ instructions.c
@@ -26,8 +26,6 @@ void handle_instruction(char *instruction, stack_t **stack,
 			swap(stack, line_number);
 		else if (strcmp(opcode, "add") == 0)
 			add(stack, line_number);
-		else if (strcmp(opcode, "nop") == 0)
-			nop(stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
