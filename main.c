997d78be5b6f282cdc7e0629b2579d3c7b6cbd87
Siskosee <seeskosee@seeskoseesko.com>
2023-12-15 03:46:10 -0800

    program entry

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * main - Entry point for the Monty interpreter.
+ *
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: %s file\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+		handle_instruction(line, &stack, line_number);
+	}
+
+	free(line);
+	fclose(file);
+
+	return (0);
+}
