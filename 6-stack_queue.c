37cf4cd7b5dbbdae82feb3bc6d00003cd680c0b2
Siskosee <seeskosee@seeskoseesko.com>
2023-12-15 17:51:44 -0800

    stack-queue opcodes

---
+++ 6-stack_queue.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/*
+ * op_stack - Sets the mode to STACK.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the script file
+ * where the instruction appears.
+ */
+void op_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	/* Setting the mode to STACK */
+        mode = STACK;
+}
+
+/*
+ * op_queue - Sets the mode to QUEUE.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the script file
+ * where the instruction appears.
+ */
+void op_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	/* Setting the mode to QUEUE */
+	mode = QUEUE;
+}
