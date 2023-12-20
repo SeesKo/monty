0024eaaf8579dcbda5dc83e4b9611bc88bca3eda
Siskosee <seeskosee@seeskoseesko.com>
2023-12-20 08:20:56 -0800

    freeing stack

---
+++ free_stk.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * free_stack - Frees the entire stack.
+ * @stack: Pointer to the stack.
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
+
+	while (stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+}
